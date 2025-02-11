98a561298914da9fb9f47f6d1a722281ebc657fd
Gaurav Jain <gauravrameshchandr.j@dish.com>
2025-02-11 09:21:35 +0530

    AI model up and running

---
+++ index.js
@@ -0,0 +1,43 @@
+import  { ChatGoogleGenerativeAI }  from "@langchain/google-genai";
+import express from "express";
+import dotenv from 'dotenv';
+import { ReadableStream } from "stream/web";
+
+dotenv.config();
+console.log("API key val: ", process.env.GOOGLE_API_KEY)
+const model = new ChatGoogleGenerativeAI({
+    model: "gemini-pro",
+    maxOutputTokens: 2048,
+    apiKey: process.env.GOOGLE_API_KEY, // Explicitly pass the API key
+  });
+
+const app = express()
+app.use(express.json())
+
+app.post('/api/prompts', async(req, res) => {
+  const { prompt } = req.body
+  try {
+    const response = await generateResponse(prompt)
+    res.status(200).json({response: response})
+  } catch (error) {
+    res.status(500).json({ error: error.message})
+    console.error(error)
+  }
+})
+
+
+async function generateResponse(prompt) {
+  try {
+    const response = await model.invoke(prompt)
+    console.log(response.content)
+    return response.content
+  } catch (error) {
+    console.error(error);
+    return error.message
+  }
+}
+
+
+app.listen(4000, () => {
+  console.log('SERVER RUNNING ON PORT:4000')
+})
